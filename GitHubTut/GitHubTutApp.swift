c8dfa6c464726783ad183380891df2ce7dd731a9
Andrew Mertzelos <a.mertzelos@gmail.com>
2023-06-13 23:59:09 +0300

    Initial Commit

---
+++ GitHubTut/GitHubTutApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTutApp.swift
+//  GitHubTut
+//
+//  Created by antreas mertzelos on 13/6/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
