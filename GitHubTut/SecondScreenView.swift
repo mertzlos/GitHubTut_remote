6a68d24ad0cd59273b9e5e8a45126c74b0375633
Andrew Mertzelos <a.mertzelos@gmail.com>
2023-06-14 00:12:56 +0300

    created second view

---
+++ GitHubTut/SecondScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondScreenView.swift
+//  GitHubTut
+//
+//  Created by antreas mertzelos on 14/6/23.
+//
+
+import SwiftUI
+
+struct SecondScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondScreenView()
+    }
+}
