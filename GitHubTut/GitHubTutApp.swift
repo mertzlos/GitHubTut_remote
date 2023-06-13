0651fba3ebde48ee51be216b0c5c0da84564cd85
Andrew Mertzelos <a.mertzelos@gmail.com>
2023-06-14 00:03:51 +0300

    changed contentView to mainView

--- GitHubTut/GitHubTutApp.swift
+++ GitHubTut/GitHubTutApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GitHubTutApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
--- GitHubTut/MainView.swift
+++ GitHubTut/MainView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         VStack {
            
@@ -19,6 +19,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
